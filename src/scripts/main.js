962d28f4be3504be594c8db04dc44c2c1ec227a3
Lucas Fernandes <lucasfeitosa8@gmail.com>
2025-04-20 15:31:22 -0300

    (feat): Finalizada estilização do header e do carousel de lancamentos para dispostivos de 1024px

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,4 @@
-// ### A loǵica abaixo será utilizada para apresentar o resultado da busca do campo de texto
+// ### A loǵica abaixo será utilizada para apresentar o resultado da busca do campo de texto em dispositivos mobile
 let buscaItens = document.querySelector("#busca-produtos");
 let iconeLupaBusca = document.querySelector("#lupa-busca");
 let resultadoBusca = document.querySelector(".resultado-busca");
@@ -18,5 +18,27 @@ iconeLupaBusca.addEventListener('click', () => {
         menuELogoContainer.style.marginTop = '.4em';
         buscaItens.style.marginTop = '.4em'
         carouselContainer.style.marginTop = '1em';
+    }
+})
+
+
+// A lógica abaixo será utilizada para apresentar o resultado da busca do campo de texto em dispositivos desktop
+let itemBuscaDesktop = document.querySelector("#busca-produtos-desktop");
+let iconeLupaDesktop = document.querySelector("#lupa-busca-desktop");
+let resultadoBuscaDesktop = document.querySelector(".resultado-busca-desktop");
+let menuSuperior = document.querySelector(".menu-superior");
+
+iconeLupaDesktop.addEventListener('click', () => {
+    let resultadoDesktop = itemBuscaDesktop.value;
+
+    if(resultadoDesktop == '') {
+        resultadoBuscaDesktop.textContent = '';
+    }
+
+    if(resultadoDesktop != '') {
+        resultadoBuscaDesktop.textContent = `Você buscou por: '${resultadoDesktop}'`;
+        resultadoBuscaDesktop.style.marginLeft = '10.9em';
+        resultadoBuscaDesktop.style.marginTop = '.7em';
+        resultadoBuscaDesktop.style.marginBottom = '.1em';
     }
 })
